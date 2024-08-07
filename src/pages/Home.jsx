1ab88ef7fd9fcac839e5f4619d3be9a568c82a4b
NataponJ <nataponj@gmail.com>
2024-08-07 16:13:45 +0700

    finished BackButton and ShowBook Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -39,15 +39,15 @@ export const Home = () => {
         <table className="w-full border-separate border-spacing-2">
           <thead>
             <tr>
-              <th className="border border-s-teal-600 rounded-md">No</th>
-              <th className="border border-s-teal-600 rounded-md">Title</th>
-              <th className="border border-s-teal-600 rounded-md max-md:hidden">
+              <th className="border border-slate-600 rounded-md">No</th>
+              <th className="border border-slate-600 rounded-md">Title</th>
+              <th className="border border-slate-600 rounded-md max-md:hidden">
                 Author
               </th>
-              <th className="border border-s-teal-600 rounded-md max-md:hidden">
+              <th className="border border-slate-600 rounded-md max-md:hidden">
                 Publish Year
               </th>
-              <th className="border border-s-teal-600 rounded-md">
+              <th className="border border-slate-600 rounded-md">
                 Operations
               </th>
             </tr>
