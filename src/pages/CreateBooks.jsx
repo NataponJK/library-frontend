dcdcb34b0d5b1832f91f52921bc06fa14276194c
NataponJ <nataponj@gmail.com>
2024-08-07 20:10:41 +0700

    finished editBook Page & change type of publishYear to number in CreateBooks Page

--- src/pages/CreateBooks.jsx
+++ src/pages/CreateBooks.jsx
@@ -32,35 +32,35 @@ export const CreateBooks = () => {
   };
 
   return (
-    <div className='p-4'>
+    <div className="p-4">
       <BackButton />
-      <h1 className='text-3xl my-4'>Create Book</h1>
-      {loading ? <Spinner /> : ''}
-      <div className='flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto'>
-        <div className='my-4'>
-          <label className='text-xs mr-4 text-gray-500'>Title</label>
-          <input 
-            type='text'
+      <h1 className="text-3xl my-4">Create Book</h1>
+      {loading ? <Spinner /> : ""}
+      <div className="flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto">
+        <div className="my-4">
+          <label className="text-xs mr-4 text-gray-500">Title</label>
+          <input
+            type="text"
             value={title}
-            onChange={(e)=> setTitle(e.target.value)}
+            onChange={(e) => setTitle(e.target.value)}
             className="border-2 border-gray-500 px-4 py-2 w-full"
           />
         </div>
-        <div className='my-4'>
-          <label className='text-xs mr-4 text-gray-500'>Author</label>
-          <input 
-            type='text'
+        <div className="my-4">
+          <label className="text-xs mr-4 text-gray-500">Author</label>
+          <input
+            type="text"
             value={author}
-            onChange={(e)=> setAuthor(e.target.value)}
+            onChange={(e) => setAuthor(e.target.value)}
             className="border-2 border-gray-500 px-4 py-2 w-full"
           />
         </div>
-        <div className='my-4'>
-          <label className='text-xs mr-4 text-gray-500'>Publish Year</label>
-          <input 
-            type='text'
+        <div className="my-4">
+          <label className="text-xs mr-4 text-gray-500">Publish Year</label>
+          <input
+            type="number"
             value={publishYear}
-            onChange={(e)=> setPublishYear(e.target.value)}
+            onChange={(e) => setPublishYear(e.target.value)}
             className="border-2 border-gray-500 px-4 py-2 w-full"
           />
         </div>
@@ -69,5 +69,5 @@ export const CreateBooks = () => {
         </button>
       </div>
     </div>
-  )
+  );
 };
